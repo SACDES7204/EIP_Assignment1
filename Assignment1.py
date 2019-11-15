8ac383a9512b55436efc70204d738a04f301b727
SACDES7204 <42696484+SACDES7204@users.noreply.github.com>
2019-11-15 17:25:55 +0530

    With 99% Accuracy

--- Assignment1.py
+++ Assignment1.py
@@ -1,6 +1,7 @@
 # https://keras.io/
 !pip install -q keras
 import keras
+
 import numpy as np
 
 from keras.models import Sequential
@@ -9,42 +10,87 @@ from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 
 from keras.datasets import mnist
+
+"""### Load pre-shuffled MNIST data into train and test sets"""
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# Commented out IPython magic to ensure Python compatibility.
 print (X_train.shape)
 from matplotlib import pyplot as plt
-%matplotlib inline
+# %matplotlib inline
 plt.imshow(X_train[0])
+
 X_train = X_train.reshape(X_train.shape[0], 28, 28,1)
 X_test = X_test.reshape(X_test.shape[0], 28, 28,1)
+
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
+
 y_train[:10]
+
+# Convert 1-dimensional class arrays to 10-dimensional class matrices
 Y_train = np_utils.to_categorical(y_train, 10)
 Y_test = np_utils.to_categorical(y_test, 10)
+
 Y_train[:10]
+
 from keras.layers import Activation
+
+model = Sequential()
+ 
+# model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(1,28,28)))
+# model.add(Convolution2D(32, 3, 3, activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2,2)))
+# model.add(Dropout(0.25))
+ 
+# model.add(Flatten())
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(10, activation='softmax'))
+
+
 model = Sequential()
 model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(28,28,1)))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+
+
 model.add(Convolution2D(10, 1, activation='relu'))
-model.add(Convolution2D(10, 26))
+model.add(Convolution2D(10, 18))
 model.add(Flatten())
 model.add(Activation('softmax'))
+
 model.summary()
+
 model.compile(loss='categorical_crossentropy',
              optimizer='adam',
              metrics=['accuracy'])
+
+model.fit(X_train, Y_train, batch_size=32, nb_epoch=10, verbose=1)
+
 score = model.evaluate(X_test, Y_test, verbose=0)
+
 print(score)
+
 y_pred = model.predict(X_test)
+
+
+
 print(y_pred[:9])
 print(y_test[:9])
+
 layer_dict = dict([(layer.name, layer) for layer in model.layers])
+
+# Commented out IPython magic to ensure Python compatibility.
 import numpy as np
 from matplotlib import pyplot as plt
 from keras import backend as K
-%matplotlib inline
+# %matplotlib inline
 # util function to convert a tensor into a valid image
 def deprocess_image(x):
     # normalize tensor: center on 0., ensure std is 0.1
